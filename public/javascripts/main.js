18c2bb2c903f643c8d46dfda66ac92f4e739d05f
Hung Nguyen <nhuyhung146@gmail.com>
2022-12-06 23:26:36 +0700

    update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,9 @@
 var socket = io("http://localhost:3000");
+const message = document.querySelector('.message')
+const submitBtn = document.querySelector('.submit')
+const form = document.querySelector('#form')
+
+let prevDate = 0
 
 //client receive data from server
 socket.on("server-update-data", function(data) {
@@ -7,4 +12,18 @@ socket.on("server-update-data", function(data) {
     $('#currentCH4').html(data.ch4);
     $('#currentGas').html(data.gas);
     $('#currentCO').html(data.co);
+
+    let date = new Date()
+
+    if (+data.co > 1 && date.getTime() - prevDate > 60) {
+        console.log(date.getTime() - prevDate);
+        prevDate = date.getTime()
+        message.value = `
+        Nhiệt độ: ${data.temp}
+        Độ ẩm: ${data.humi}
+        CO: ${data.co}
+        CH4: ${data.ch4}
+        GAS: ${data.gas}`
+        // submitBtn.click()
+    };
 });
